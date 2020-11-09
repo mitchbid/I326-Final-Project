b4e7e5ece92e080f95015a3b7e910f59032b74d5
Nur <nmuluget@umd.edu>
2020-11-09 16:12:46 -0500

    project skeleton

---
+++ restaurant_ratings_generator.py
@@ -0,0 +1,59 @@
+""" Mitchell Biderberg, Nasir Zaidi, Nkunim Antwi, Nur Mulugeta
+"""
+
+
+"""Final Project: Resturant Ratings Generator  """
+
+class Yelp_Data: 
+ """ A class that reads datafile and iterates through
+data types """
+    def __init__():
+    """ a function to initilize our variables and 
+    attributes of the class"""
+    
+    def __read__():
+        """ a function to read the data 
+        file and assigns it to a dicitonary
+        """
+    
+class Yelp_Ratings:
+    """ a class that idirates through
+    the dictionaries, creates a rating system """
+    
+    def __ratingsystem__():
+        """
+        A function that creates a rating system. 
+        
+        """
+        
+    def __inputrating__():
+        """
+        A function that takes the created 
+        rating system and compares to user input
+        """
+    def __ratingresults_():
+        """
+        A function that displays the return 
+        of inputrating
+        """
+class User_Interface: 
+    """
+    A class that has a user interface to take input 
+    on location, cuisine and price
+    """
+    def __location__():
+        """
+        a function that stores user input on 
+        their location
+        
+        """
+    def __cusine__():
+        """
+        a function that stores user input on their cusine 
+        prefrence
+        """
+    def __price__():
+        """
+        a function that stores user input on their 
+        price prefrence
+        """ 
