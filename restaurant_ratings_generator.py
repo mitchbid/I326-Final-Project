1fe571efa0b36df36aa619fae61a294ba9510571
Nasir Zaidi <nasirzaidi105@gmail.com>
2020-11-30 12:17:04 -0500

    added new info

--- restaurant_ratings_generator.py
+++ restaurant_ratings_generator.py
@@ -1,66 +1,76 @@
-""" Mitchell Biderberg, Nasir Zaidi, Nkunim Antwi, Nur Mulugeta
-"""
+""" Mitchell Biderberg, Nasir Zaidi, Nkunim Antwi, Nur Mulugeta"""
 
 
 """Final Project: Resturant Ratings Generator  """
 
 class Yelp_Data: 
- """ A class that reads datafile and iterates through
-data types """
-    def __init__():
-    """ a function to initilize our variables and 
-<<<<<<< HEAD
-    attributes of the class
-    Side effects:
-        Sets attribute for Yelp Data dictionary"""
+    """ A class that reads datafile and iterates through
+    data types """
+    def __init__(self):
+        """ 
+        (Nur) a function to initialize our variables and reads in our data
+            Args:
+                Url (rss) - url path containing our dataset
+                Side effects: 
+                    Sets attribute for Yelp Data dictionary
+        """
         self.Yelp_Dict = {}
-=======
-    attributes of the class"""
->>>>>>> b4e7e5ece92e080f95015a3b7e910f59032b74d5
-    
-    def __read__():
-        """ a function to read the data 
-        file and assigns it to a dicitonary
-        """
-    
-class Yelp_Ratings:
-    """ a class that idirates through
-    the dictionaries, creates a rating system """
-    
-    def __ratingsystem__():
-        """
-        A function that creates a rating system. 
         
+    def value_match():
+        """
+        (Mitch) A function that Matches values from the poarsed data file to the yelp dict
+            Args: 
+                Self - refrence variable
+                Filename - refrence to file being utilized 
+            Returns:
+                data_yelp (dict) - new dictionary with matched key value pairs
+        """   
+        #data_yelp = {}
+        #for i in Yelp_Data:
+            #if x in yelp_data:
+                #x.append(data_yelp)
+                
+    def input_rating():
+        """
+        (Nasir) A function that takes input from the user based off specifications set
+            Args:
+                Location (list) - input varible taking in the user's location prefrence
+                Cuisine (list) - input varibale taking in the users cuisine prefrences
+                Price (list) - input variable taking in the users price range prefence  
+        """
+    
+    def input_parser():
+        """
+        (Nkunim) Function that takes the input values recieved from the user, parses through each list 
+        and creates a dictionary with key value pairs for each list
+            Args:
+                arglist (list of str): list of arguments from the command line.
+            Returns:
+                input_yelp (dict) - dictionary with user's input values for Location(key), Cuisine(key), Price(key)
         """
         
-    def __inputrating__():
-        """
-        A function that takes the created 
-        rating system and compares to user input
-        """
-    def __ratingresults_():
-        """
-        A function that displays the return 
-        of inputrating
-        """
-class User_Interface: 
+class match_results: 
     """
     A class that has a user interface to take input 
     on location, cuisine and price
     """
-    def __location__():
+    def dict_match():
         """
-        a function that stores user input on 
-        their location
-        
-        """
-    def __cusine__():
-        """
-        a function that stores user input on their cusine 
-        prefrence
-        """
-    def __price__():
-        """
-        a function that stores user input on their 
-        price prefrence
+        Function that takes the key value pairs from data_yelp and input_yelp and compares them, searching for any and all matches
+            Returns:
+                match_list (str) - list of data matches between the data_yelp and input_yelp dictionaries 
         """ 
+        
+def main():
+    """
+        Reads in information from our yelp data file, user input and
+        prints out information regarding user's prefrences
+    """"
+    
+def parse_args(arglist):
+"""
+Parses command line arguments 
+"""
+
+
+if __name__ == "__main__":
