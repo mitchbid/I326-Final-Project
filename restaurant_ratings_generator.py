6fb38b8f8ce13de5e32c88c61bab2330702948ed
Nasir Zaidi <nasirzaidi105@gmail.com>
2020-11-30 18:53:13 -0500

    added new info

--- restaurant_ratings_generator.py
+++ restaurant_ratings_generator.py
@@ -6,9 +6,10 @@
 class Yelp_Data: 
     """ A class that reads datafile and iterates through
     data types """
-    def __init__(self):
+    def __init__(self,filename,url):
         """ 
-        (Nur) a function to initialize our variables and reads in our data
+        (Nur) a function to initialize our variables and reads in our data,
+        creates two dataframes
             Args:
                 Url (rss) - url path containing our dataset
                 Side effects: 
@@ -16,11 +17,10 @@ class Yelp_Data:
         """
         self.Yelp_Dict = {}
         
-    def value_match():
+    def df_merge(self):
         """
-        (Mitch) A function that Matches values from the poarsed data file to the yelp dict
+        (Nasir) A function that reads in aggr
             Args: 
-                Self - refrence variable
                 Filename - refrence to file being utilized 
             Returns:
                 data_yelp (dict) - new dictionary with matched key value pairs
@@ -30,47 +30,47 @@ class Yelp_Data:
             #if x in yelp_data:
                 #x.append(data_yelp)
                 
-    def input_rating():
+    def input_rating(self):
         """
         (Nasir) A function that takes input from the user based off specifications set
+        then validates each of these values (calling input_parser after each input)
             Args:
                 Location (list) - input varible taking in the user's location prefrence
                 Cuisine (list) - input varibale taking in the users cuisine prefrences
                 Price (list) - input variable taking in the users price range prefence  
+                
         """
     
     def input_parser():
         """
         (Nkunim) Function that takes the input values recieved from the user, parses through each list 
-        and creates a dictionary with key value pairs for each list
+        and validates each value (location,Price, Cuisine) ---> Each has its own function
             Args:
                 arglist (list of str): list of arguments from the command line.
             Returns:
-                input_yelp (dict) - dictionary with user's input values for Location(key), Cuisine(key), Price(key)
+                            (tuple) - of all specifications
+                input_yelp (df) - dictionary with user's input values for Location(key), Cuisine(key), Price(key)
         """
-        
-class match_results: 
-    """
-    A class that has a user interface to take input 
-    on location, cuisine and price
-    """
-    def dict_match():
+    def df_match():
         """
         Function that takes the key value pairs from data_yelp and input_yelp and compares them, searching for any and all matches
             Returns:
                 match_list (str) - list of data matches between the data_yelp and input_yelp dictionaries 
         """ 
         
+    def data_visualizer():
+        """
+        Using Pandas print out the df produced in df_match()
+        """
+        
+        
 def main():
     """
-        Reads in information from our yelp data file, user input and
-        prints out information regarding user's prefrences
+    (Mitch) Instansiate class and invoke methods listed above
     """"
     
 def parse_args(arglist):
-"""
-Parses command line arguments 
-"""
-
-
+    """
+    (Nkunim) Parses command line arguments 
+    """
 if __name__ == "__main__":
